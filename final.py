d067d3b0667ae673f5db10804a4466cf7126e697
Lykos2 <pasunti.prasanjith@pilani.bits-pilani.ac.in>
2024-02-06 20:35:14 +0000

    added_chain

--- final.py
+++ final.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import torch 
-from langchain_core.messages import AIMessage,HumanMessage
+import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
-
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
 #need to do from first
 
@@ -19,17 +23,50 @@ device_type="cuda" if torch.cuda.is_available() else "cpu"
 
 
 #only single page we have to make this for multiple html  pages 
-
 def get_vectorstore_from_url(url):
+    # get the text in document form
     loader = WebBaseLoader(url)
-    doc = loader.load()
+    document = loader.load()
+    
+    # split the document into chunks
     text_splitter = RecursiveCharacterTextSplitter()
-    document_chunks = text_splitter.split_documents(doc)
-    vector_store=Chroma.from_documents(document_chunks,)
-    return document_chunks
+    document_chunks = text_splitter.split_documents(document)
+    
+    # create a vectorstore from the chunks
+    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+
+    return vector_store
 
 
+def get_context_retriever_chain(vector_store):
+    llm = ChatOpenAI()
+    
+    retriever = vector_store.as_retriever()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+      ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+    
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+    
+    return retriever_chain
 
+
+def get_conversational_rag_chain(retriever_chain): 
+    
+    llm = ChatOpenAI()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+    ])
+    
+    stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
+    
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
 
 def get_response(user_input):
     return "I dont know"
